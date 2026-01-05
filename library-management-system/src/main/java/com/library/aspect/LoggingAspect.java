f3d63751666d90ea0e0f2acdb6d97310165312e6
Md. Reazul Haque <37473674+reazulhaque20@users.noreply.github.com>
2026-01-05 22:08:03 +0600

    update

--- library-management-system/src/main/java/com/library/aspect/LoggingAspect.java
+++ library-management-system/src/main/java/com/library/aspect/LoggingAspect.java
@@ -22,7 +22,6 @@ public class LoggingAspect {
 
     @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
     public void controllerPointcut() {
-        // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
     @Around("controllerPointcut()")
